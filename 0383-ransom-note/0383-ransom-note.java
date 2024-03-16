3a30c0046058e74d31828f4ab92f0840a4226c40
WikkiE <70148919+Wikkiee@users.noreply.github.com>
2024-03-16 20:58:11 +0530

    Time: 790 ms (5.24%), Space: 45.9 MB (6.49%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] rArr = ransomNote.toCharArray();
+        char[] mArr = magazine.toCharArray();
+
+
+        for(int i = 0;i<mArr.length;i++){
+            for(int j = 0;j<rArr.length;j++){
+            if( rArr[j] == mArr[i]){
+                rArr[j] = '0';
+                mArr[i] = '1';
+            }
+            }
+        }
+        for(char c : mArr){
+            System.out.print(c + " ");
+        }
+        System.out.println();
+        for(char c : rArr){
+            if(c != '0' ){
+                return false;
+            }
+        }
+        return true;
+    }
+}
